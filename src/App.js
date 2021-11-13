74473a0457bf0d633a43dd946e5774ede0a0f01e
Deepak0844 <deepakram0105@gmail.com>
2021-11-13 12:47:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import Rating from "@mui/material/Rating";
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { useState } from "react";
-
-
-
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import React, { useState} from "react";
 
 export default function App(){
-  const shoppingProducts =[
+  const [cart, setCart] = useState([])
+
+  //product list
+    const shoppingProducts =[
     {
       brand:"Nike",
       name:"Men's Air Max Excee Running Shoes",
       image:"https://m.media-amazon.com/images/I/81Wt+uytSLL._AC_UL320_.jpg",
       price:"3,336",
+      id: 1,
+      cart: false,
       rating:4.5,
     },
     {
@@ -21,6 +22,8 @@ export default function App(){
       name:"Men's Renew Ride Running Shoes",
       image:"https://m.media-amazon.com/images/I/61iK-U9enSL._AC_UL320_.jpg",
       price:"2,217",
+      id: 2,
+      cart: false,
       rating:4,
     },
     {
@@ -28,6 +31,8 @@ export default function App(){
       name:"Women's Flex Fury 2 Running Shoes",
       image:"https://m.media-amazon.com/images/I/719GdOZCGAL._AC_UL320_.jpg",
       price:"3,305",
+      id: 3,
+      cart: false,
       rating:4.3,
     },
     {
@@ -35,6 +40,8 @@ export default function App(){
       name:"mens Nike Todos Running Shoes",
       image:"https://m.media-amazon.com/images/I/71YWn0X2NQL._AC_UL320_.jpg",
       price:"3,355",
+      id: 4,
+      cart: false,
       rating:4.25,
     },
     {
@@ -42,6 +49,8 @@ export default function App(){
       name:"Men Downshifter 8 Running Shoes",
       image:"https://m.media-amazon.com/images/I/61HGV9gCslL._AC_UL320_.jpg",
       price:"3,515",
+      id: 5,
+      cart: false,
       rating:4.5,
     },
     {
@@ -49,6 +58,8 @@ export default function App(){
       name:"Men's Renew Arena Running Shoes",
       image:"https://m.media-amazon.com/images/I/81KJkXzrB0L._AC_UL320_.jpg",
       price:"3,614",
+      id: 6,
+      cart: false,
       rating:4.5,
     },
     {
@@ -56,6 +67,8 @@ export default function App(){
       name:"mens Nike Zoom Winflo 7 Running Shoe",
       image:"https://m.media-amazon.com/images/I/81sp+eTUd-S._AC_UL320_.jpg",
       price:"5,698",
+      id: 7,
+      cart: false,
       rating:3.8,
     },
     {
@@ -63,98 +76,105 @@ export default function App(){
       name:"Men's Air Max Vg-r Running Shoe",
       image:"https://m.media-amazon.com/images/I/71GuRh1JRaL._AC_UL320_.jpg",
       price:"6,556",
+      id: 8,
+      cart: false,
       rating:4,
     },
   ]
+
+  const [products] = useState(shoppingProducts)
+
+  //add to cart
+const addtocart = (item) => {
+    let cart2 = [...cart]
+    cart2.push({ ...item })
+    products.map((i) => {
+      if (item.id === i.id) {
+        return i.cart = true //if item added in the cart, it returns and hides add to cart button 
+      }else{
+        return setCart(cart2) //if item not added in the cart, it it shows add to cart button 
+      }
+  })
+  }
+  //remove from cart
+const removetocart = (item) =>{
+    let cart2 = cart.filter((i) => i.id !== item.id)
+    products.map((i) => {
+      if (item.id === i.id) {
+        return i.cart = false // if item already in cart, removes item when remove button clicked
+      }else{
+        return setCart(cart2) 
+      }
+    })
+}
+// to total the no of item added in cart
+const total = () => {
+    let x = 0
+    cart.map((i) => {
+        return x+=1 
+    })
+    return x //if cart is empty it shows default value 0
+  }
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="productList">
- {shoppingProducts.map((li,index)=><Products key={index} brand={li.brand} name={li.name} image={li.image} price={li.price} rating={li.rating}/>)}
- </div>
-    </div>
-);}
-
-function Products({brand,image,name,price,rating}){
-  return(
-  <section className="products">
-      <img className="productImg" src={image} alt={name}/>
-       <p><b>{brand}</b></p>
-       <p>{name}</p>
-       <Rating name="half-rating-read" defaultValue={rating}precision={0.1}readOnly/>
-      <p className="pricing">₹ {price}</p>
-      {/* <Button className="btnAddCart" variant="contained">Add to cart</Button> */}
-      <div className="btn-cart">
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
-  <Button className="btnChange">+1</Button>
-  <Button className="btnChange">-1</Button>
-</ButtonGroup>
-<Counter/>
-</div>
-  </section>
-  )}
-
-  function Counter() {
-    const [add, setAdd] = useState(0);
-  
-    return (
-      <div>
-        <button className="button" onClick={() => setAdd(add + 1)}>👍 {add}</button>
-        <button className="button" onClick={() => setAdd(add - 1)}>👎</button>
-        {/* <ButtonGroup variant="outlined" aria-label="outlined button group">
-    <Button onClick={handleClick1} className="btnChange">+1{count}</Button>
-    <Button onClick={handleClick2} className="btnChange">-1</Button>
-  </ButtonGroup> */}
-      </div>
-    );
-  }
-
-
-function Navbar() {
-
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+   <div className='containerBox'>
+     <div>
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <p className="navbar-brand">Navbar</p>
+          <ul className="navbar-brand">Nike</ul>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <p className="nav-link active" aria-current="page">Home</p>
-                </li>
-                <li className="nav-item">
-                  <p className="nav-link">About</p>
-                </li>
-                <li className="nav-item dropdown">
-                  <p className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Shop
-                  </p>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><p className="dropdown-item">All Products</p></li>
-                    <li><hr className="dropdown-divider"></hr></li>
-                    <li><p className="dropdown-item">Popular Items</p></li>
-                    <li><p className="dropdown-item">New Arrivals</p></li>
-                  </ul>
-                </li>
-              </ul>
-              <form className="d-flex">
-                <button className="btn btn-outline-success" type="submit"><i className="fa fa-shopping-cart"></i> Cart <span></span></button>
-              </form>
+               <span className="navbar-toggler-icon"></span>
+             </button>
+             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                 <li className="nav-item">
+                   <p className="nav-link active" aria-current="page">Home</p>
+                 </li>
+                 <li className="nav-item">
+                   <p className="nav-link active">About</p>
+                 </li>
+                 <li className="nav-item dropdown">
+                  <p className="nav-link active dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     Shop
+                   </p>
+                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                     <li><p className="dropdown-item">All Products</p></li>
+                     <li><hr className="dropdown-divider"></hr></li>
+                     <li><p className="dropdown-item">Popular Items</p></li>
+                     <li><p className="dropdown-item">New Arrivals</p></li>
+                   </ul>
+                 </li>
+               </ul>
+               <button className="btn btn-outline-success cartBtn"> <ShoppingCartIcon/> Cart {total()}
+     </button>
             </div>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-  
-
-
-
-  
-
-
-
-
+           </div>
+         </nav>
+       </div>
+   <main className='productDisplay'>
+        {products.map((item,index) => (
+          <section className="products"  key={index}>
+                 <img className="productImg" src={item.image} alt={item.name}/>
+                 <p><b>{item.brand}</b></p>
+                 <p>{item.name}</p>
+                 <Rating name="half-rating-read" defaultValue={item.rating}precision={0.1}readOnly/>
+                <p className="pricing">₹ {item.price}</p>
+                {//if item is not in cart it shows add to cart button
+                  item.cart === false
+                  &&
+                  <button className='btn btn-primary' onClick={() => addtocart(item)}>
+                    Add to cart
+                </button>
+                }
+                {//if item in cart it shows remove button
+                  item.cart === true
+                  &&
+                <button className='btn btn-danger'onClick={() => removetocart(item)}>
+                  Remove
+              </button>
+          }
+       </section>
+))}
+    </main>
+</div>
+);
+}
